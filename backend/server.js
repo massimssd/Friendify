2fe21b5938901340c6e35cc3cac3a53114771c6d
Messaoudi <massi.messaoudi.123@gmail.com>
2024-07-10 18:01:01 +0200

    connexion à la base de donnée

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,42 @@
-const http = require('http');
+// backend/server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const pool = require('./controllers/db');
+require('dotenv').config({ path: './.env' });
 
-const port = process.env.PORT || 3000
+const app = express();
+app.use(bodyParser.json());
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
+// Route de base pour vérifier que le serveur fonctionne
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur Friendify!');
 });
 
-server.listen(port, () => {
-    console.log(`Serveur démarré sur http://127.0.0.1:${port}/`);
+app.get('/users', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        res.status(200).json(result.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+app.post('/users', async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        const result = await pool.query(
+            'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *',
+            [username, email, password]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://127.0.0.1:${PORT}/`);
 });
