4255d0cb0cfb4998b6e2446edc528c398548e94e
Messaoudi <massi.messaoudi.123@gmail.com>
2024-07-28 22:11:38 +0200

    connection entre front et back et signup et login

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,11 @@ require('dotenv').config();
 const userRouter = require('./routes/allrouter');
 const app = express();
 
-app.use(bodyParser.json()); // Ajoutez cette ligne pour analyser les requêtes JSON
+app.use(bodyParser.json());
+const cors = require('cors');
 
+
+app.use(cors());
 app.use('/', userRouter);
 
 const PORT = process.env.PORT || 5000;
